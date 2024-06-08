0f099901610250406bbdc97ed0c2cf10881ff662
huaxuefeng <2200012814@stu.pku.edu.cn>
2024-06-08 21:38:56 +0800

    finish file function in level 7

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include "./eval_env.h"
 #include "./parser.h"
@@ -17,26 +18,48 @@ struct TestCtx {
         return result->toString();
     }
 };
-
-int main() {
-
+/*
+std::string readFromFile(const std::string& filename){
+    std::ifstream file(filename);
+    if(!file.is_open())//input文件不存在，退出
+        std::exit(1);
+    std::string s;
+    char x;
+    while(file.get(x))
+        s += x;
+    return s;
+}*/
+//./bin/mini_lisp D:\mini_lisp\src\test.txt
+int main(int argc, char* argv[]){
+    //RJSJ_TEST(TestCtx, Lv2, Lv3, Lv4, Lv5, Lv5Extra, Lv6, Lv7, Lv7Lib, Sicp);
     EvalEnvPtr env=EvalEnv::createGlobal();//全局求值环境
+    bool read_from_file=false;
+    std::ifstream file;
+    if(argc==2){
+        file=std::ifstream(argv[1]);
+        if(file.is_open())
+            read_from_file=true;
+        else std::exit(1);
+    }
+    std::istream& input=(read_from_file ? file : std::cin);
     while (true) {
         try {
-            std::cout << ">>> ";
-            std::string line;
-            std::getline(std::cin, line);
-            if(line=="")
-                continue;
-            if (std::cin.eof()) {
+            if(!read_from_file)
+                std::cout << ">>> ";
+            if (input.eof()) {
                 std::exit(0);
             }
+            std::string line;
+            std::getline(input, line);
+            //std::cout<<line<<'\n';
+            if(line=="")
+                continue;
             auto tokens = Tokenizer::tokenize(line);
             Parser parser(std::move(tokens));  // TokenPtr不支持复制，用移动
             auto value = parser.parse();
-            //EvalEnv env;
             auto result = env->eval(std::move(value));
-            std::cout << result->toString() << std::endl;
+            if(!read_from_file)
+                std::cout << result->toString() << std::endl;
         } catch (std::runtime_error& e) {
             std::cerr << "Error: " << e.what() << std::endl;
         }
